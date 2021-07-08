6c8b5d69cd4a6f3f35bcb37d8307900ec7d7ce60
Reinaldy Rafli <aldy505@tutanota.com>
2021-07-08 13:01:23 +0700

    public dir config

--- vue/vite.config.js
+++ vue/vite.config.js
@@ -1,8 +1,9 @@
 import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
-import WindiCSS from 'vite-plugin-windicss';
+import windiCSS from 'vite-plugin-windicss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), WindiCSS()],
+  publicDir: 'public',
+  plugins: [vue(), windiCSS()],
 });
