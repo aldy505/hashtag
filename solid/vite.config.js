04d4383b4cd294b28d13f35d150f31a37300d736
Reinaldy Rafli <aldy505@tutanota.com>
2021-07-08 14:56:52 +0700

    move static folder

--- solid/vite.config.js
+++ solid/vite.config.js
@@ -3,6 +3,7 @@ import solidPlugin from 'vite-plugin-solid';
 import WindiCSS from 'vite-plugin-windicss';
 
 export default defineConfig({
+  publicDir: 'public',
   plugins: [solidPlugin(), WindiCSS()],
   build: {
     target: 'esnext',
